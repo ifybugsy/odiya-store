f4bd76bd73f23b84afe674cca58a14ce12449b01
Ifeanyi david Onyejiaka <bugsynero@yahoo.com>
2025-11-10 07:32:28 -0800

    changed the route/upload.js

--- backend/routes/upload.js
+++ backend/routes/upload.js
@@ -73,7 +73,7 @@ router.post(
         return res.status(400).json({ error: "No file uploaded" })
       }
 
-      const baseUrl = process.env.API_BASE_URL || `https://bugsymart.shop/`
+      const baseUrl = process.env.API_BASE_URL || `https://bugsymart.shop`
       const filePath = `${baseUrl}/uploads/${req.file.filename}`
 
       console.log("[upload] File uploaded successfully:", {
